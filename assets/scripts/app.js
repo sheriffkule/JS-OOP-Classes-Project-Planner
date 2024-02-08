9d9101245a338f66c2e2bcbe75c1ccdb344e3b97
sheriffkule <zeljko225@yahoo.com>
2024-02-08 13:44:13 +0100

    Creating Project list & parsing element data, "Switch Project" Logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,49 @@
-class Tooltip {
-
-}
+class Tooltip {}
 
 class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
 
+    connectMoreInfoButton() {
+        
+    }
+
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchButton = projectItemElement.querySelector('button:last-ob-type');
+        switchButton.addEventListener('click', );
+    }
 }
 
 class ProjectList {
+    projects = [];
 
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+        for (const prjItem of prjItems) {
+            this.projects.push(new ProjectItem(prjItem.id));
+        }
+        console.log(this.projects);
+    }
+
+    addProject() {}
+
+    switchProject(projectId) {
+        // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+        // this.projects.splice(projectIndex);
+        this.addProject();
+        this.projects = this.projects.filter(p => p.id !== projectId);
+    }
 }
 
 class App {
-    
+    static init() {
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+    }
 }
+
+App.init();
