dfb5ce181c1656a3a3b1343c34d4716462f8f389
sheriffkule <zeljko225@yahoo.com>
2024-02-09 18:41:58 +0100

    Handling scrolling & working with <template> tags

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,6 +9,7 @@ class DOMHelper {
         const element = document.getElementById(elementId);
         const destinationElement = document.querySelector(newDestinationSelector);
         destinationElement.append(element);
+        element.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
@@ -53,8 +54,14 @@ class Tooltip extends Component {
     create() {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
-        tooltipElement.textContent = this.text;
-        
+        const tooltipTemplate = document.getElementById('tooltip');
+        const tooltipBody = document.importNode(
+            tooltipTemplate.content,
+            true
+        );
+        tooltipBody.querySelector('p').textContent = this.text;
+        tooltipElement.append(tooltipBody);
+
         const hostElPosLeft = this.hostElement.offsetLeft;
         const hostElPosTop = this.hostElement.offsetTop;
         const hostElHeight = this.hostElement.clientHeight;
