43979d31604102b3b45bd19cbf680b83de81e6b4
sheriffkule <zeljko225@yahoo.com>
2024-02-10 16:59:01 +0100

    "preventDefault()" event propagation & "stopPropagation()" Event Delegation

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 const buttonClickHandler = (event) => {
     // event.target.disabled = true;
@@ -20,10 +20,39 @@ const buttonFn = buttonClickHandler.bind(this);
 //     button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
 
-buttons.forEach(btn => {
-    btn.addEventListener('mouseenter', buttonClickHandler)
+// buttons.forEach(btn => {
+//     btn.addEventListener('mouseenter', buttonClickHandler)
+// });
+
+// window.addEventListener('scroll', event => {
+//     console.log(event);
+// })
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
 });
 
-window.addEventListener('scroll', event => {
+const div = document.querySelector('div');
+
+div.addEventListener('click', event => {
+    console.log('CLICKED DIV');
     console.log(event);
-})
+});
+
+button.addEventListener('click', event => {
+    event.stopPropagation();
+    console.log('CLICKED BUTTON');
+    console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', event => {
+    // console.log(event.currentTarget);
+    // event.target.classList.toggle('highlight');
+    event.target.closest('li').classList.toggle('highlight');
+});
